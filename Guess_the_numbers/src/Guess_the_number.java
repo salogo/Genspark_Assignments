b33a4ed4f87209a029f7dc0727e2c8df4328e086
Zakaria Salogo <zakariasalgo@yahoo.fr>
2022-03-12 09:29:10 -0600

    refactoring the code

--- Guess_the_numbers/src/Guess_the_number.java
+++ Guess_the_numbers/src/Guess_the_number.java
@@ -2,11 +2,11 @@ import java.util.Scanner;
 import java.util.Random;
 
 public class  Guess_the_number {
-    public static void  main(String[] args) {
+    public static void  guessingNumberGame() {
 
         Scanner userInput = new Scanner(System.in);
 
-        System.out.println("Hello what is your name?");
+        System.out.println("Hello! what is your name?");
         String userName = userInput.nextLine();
         System.out.println(" Well, " + userName + " I m thinking of a number between 1 and 20 take a guess.");
 
@@ -16,37 +16,40 @@ public class  Guess_the_number {
         int value = random.nextInt(max + min) + min;
         System.out.println(value);
 
-       byte guessingNumber = userInput.nextByte();
-       int attempt =0;
-       boolean win = true;
+        int attempt = 5;
+        byte guessingNumber;
+        int i;
 
-            if (guessingNumber > value){
-               attempt ++;
-                System.out.println("high");;
-           }
-           else if (guessingNumber < value){
-               attempt ++;
-                System.out.println("your guess is low ;" + "your attempt is: " + attempt); ;
-           }
-        else if (guessingNumber == value && attempt <= 6 ){
-            attempt++;
-                System.out.println("win" + "your attempt is: " + attempt); ;
+        for (i = 0; i < attempt; i++) {
+             guessingNumber = userInput.nextByte();
+
+            if  (guessingNumber == value ) {
+                System.out.println("Good job  " + userName + "You guessed my number in "+ i +" guess" );
+                break;
+            }
+            else if (guessingNumber > value && i != attempt - 1) {
+                System.out.println("Your guess is higher, try again");
+            }
+            else if (guessingNumber < value && i != attempt - 1) {
+                System.out.println("Your guess is low try again");
+            }
+
+
         }
-        else if (guessingNumber == value && attempt >= 6) {
-            attempt++;
-                win = false;
-                System.out.println("lost " + "your attempt is: " + attempt); ;
-            }
-        else if (attempt < 6 ){
-                System.out.println("guess again");
-                byte reguess = userInput.nextByte();
-                 guessingNumber= reguess;
 
-            }
-        else {
-                System.out.println("your attempt is " + String.valueOf(attempt));
-            }
-
+        if (i == attempt) {
+            System.out.println("you attempted 6 lost ");
+        }
 
     }
+
+
+    // Driver Code
+    public static void
+    main(String arg[]) {
+        // Function Call
+        guessingNumberGame();
+    }
+
+
 }
