3e70cdf5ea8c192758a34fc3704f1dc1c18a57d6
Zakaria Salogo <zakariasalgo@yahoo.fr>
2022-03-22 16:44:55 -0500

    condition to check if player guess is correct is added

--- HangmanGame/src/Hangman.java
+++ HangmanGame/src/Hangman.java
@@ -22,23 +22,57 @@ public class Hangman {
         String word = words.get(rand.nextInt(words.size()));
 
         System.out.println(word);
+
         List<Character> Guesses = new ArrayList<>();
 
-        printWordState(word, Guesses);
-        System.out.println("Enter your letter!:");
+        while (true){
+            getPlayerGuesses(keyboard, word, Guesses);
+            if (printWordState(word, Guesses) ){
+                System.out.println("You win");
+                break;
+            }
+
+            System.out.println("Please enter your guess");
+            if (keyboard.nextLine().equals(word)){
+                System.out.println("You win");
+                break;
+            }
+            else {
+                System.out.println("please try again");
+            }
+        }
+        //System.out.println("You win");
+
 
     }
 
-    private static void printWordState(String word, List<Character> Guesses) {
-        for (int i = 0; i < word.length(); i++){
-            if (Guesses.contains(word.charAt(i))){
-                System.out.println(word.charAt(i));
+    private static void getPlayerGuesses(Scanner keyboard, String word, List<Character> Guesses) {
+        int numberGuess = 6;
+        while (numberGuess > 0){
+          printWordState(word, Guesses);
+
+          System.out.println("Enter your letter!:");
+          String letterGuess = keyboard.nextLine();
+          Guesses.add(letterGuess.charAt(0));
+          numberGuess--;
+      }
+    }
+
+
+    private static boolean printWordState(String word, List<Character> Guesses) {
+        int correctCount = 0;
+        for (int i = 0; i < word.length(); i++) {
+            if (Guesses.contains(word.charAt(i))) {
+                System.out.print(word.charAt(i));
+                correctCount++;
             }
             else {
                 System.out.print("-");
             }
         }
         System.out.println();
+
+        return (word.length() == correctCount);
 
 
 
