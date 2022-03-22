f40e023e630e7f1305cbd3ea5dbbc9fa7d647cfe
Zakaria Salogo <zakariasalgo@yahoo.fr>
2022-03-22 08:52:11 -0500

    A guessing word is added

--- HangmanGame/src/Hangman.java
+++ HangmanGame/src/Hangman.java
@@ -1,5 +1,47 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.Scanner;
+
 public class Hangman {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
+
+        Scanner scanner = new Scanner(new File("/Users/zakariasalogo/Desktop/words.txt"));
+
+        List<String> words = new ArrayList<>();
+
+        while (scanner.hasNext()) {
+            //System.out.println(scanner.nextLine());
+            words.add(scanner.nextLine());
+        }
+        Random rand = new Random();
+        // pick up random word from the list length
+        String word = words.get(rand.nextInt(words.size()));
+
+        System.out.println(word);
+        List<Character> userGuesses = new ArrayList<>();
+
+        printWordState(word, userGuesses);
+        System.out.println("Enter your letter!:");
+
+    }
+
+    private static void printWordState(String word, List<Character> userGuesses) {
+        for (int i = 0; i < word.length(); i++){
+            if (userGuesses.contains(word.charAt(i))){
+                System.out.println(word.charAt(i));
+            }
+            else {
+                System.out.print("-");
+            }
+        }
+        System.out.println();
+
+
+
+
 
     }
 }
